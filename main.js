da412f687bbb7666bbdb9f79d244b6834a43295c
Pratham Mehta <pratham.mehta@zeuslearning.com>
2015-01-23 17:42:55 +0530

    regular exp.

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global define, $, brackets */
 
-/** {ext_name} Extension 
-    description 
+/** {ext_name} Extension
+    description
 */
 define(function (require, exports, module) {
     'use strict';
-    
-    
+
+
 	var AppInit            	= brackets.getModule("utils/AppInit");
     var CodeHintManager     = brackets.getModule("editor/CodeHintManager");
     var CommandManager      = brackets.getModule('command/CommandManager');
@@ -22,10 +22,12 @@ define(function (require, exports, module) {
 	var PreferencesManager	= brackets.getModule('preferences/PreferencesManager');
 	var Menus          		= brackets.getModule("command/Menus");
 	var MainViewManager		= brackets.getModule("view/MainViewManager");
-    
-    
-    var LOAD_SCREEN_REG_EXP = /\bloadScreen\b\(/;
-    
+
+
+    var LOAD_SCREEN_REG_EXP = /\bloadScreen\b(\(')|\bloadScreen\b(\(")/;
+
+    var GET_MESSAGE_REG_EXP = /\bgetMessage\b(\')|\bgetMessage\b(\")|\bgetAccMessage\b(\(')|\bgetAccMessage\b(\(")|\bsetMessage\b(\(')|\bsetMessage\b(\(")|\bsetAccMessage\b(\(')|\bsetAccMessage\b(\(")/;
+
     AppInit.appReady(function () {
 //		require('hints');
 
@@ -45,19 +47,25 @@ define(function (require, exports, module) {
     function getScreenHints(){
         console.log('screen hints');
     }
-    
-    function getElementIdHints(){}
-    
+
+    function getElementIdHints(){
+        console.log('element hints');
+    }
+
     function handleKey(event){
         var editor  = EditorManager.getCurrentFullEditor();
-        
+
         if ((event.type === 'keydown' /*&& event.keyCode === KeyEvent.DOM_VK_TAB*/) ||
 			(event.type === 'keyup' /*&& event.keyCode === KeyEvent.DOM_VK_RETURN*/)) {
             var currentLineNum = editor.getCursorPos().line;
-            
+
             var currentLine = editor.document.getLine(currentLineNum);
             if(currentLine.match(LOAD_SCREEN_REG_EXP)){
                 getScreenHints();
+            }
+
+            if(currentLine.match(GET_MESSAGE_REG_EXP)){
+                getElementIdHints();
             }
         }
     }
